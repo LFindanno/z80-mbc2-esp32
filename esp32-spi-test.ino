f95d7afaecf35915677ec2b3116c8c815bea6bec
luigi <lfindanno@yahoo.it>
2022-04-10 11:02:09 +0200

    Add files via upload

---
+++ esp32-spi-test.ino
@@ -0,0 +1,52 @@
+// Luigi Findanno 09/04/2022
+// On SPI command, set a LED ON/OFF
+
+#include <ESP32SPISlave.h>
+
+ESP32SPISlave slave;
+
+static constexpr uint32_t BUFFER_SIZE {32};
+uint8_t spi_slave_tx_buf[BUFFER_SIZE];
+uint8_t spi_slave_rx_buf[BUFFER_SIZE];
+byte a,c,d,f=0;
+void setup() {
+    pinMode(12, OUTPUT);
+    pinMode(22, OUTPUT); // LED
+    Serial.begin(115200);
+    delay(2000);
+
+    // begin() after setting
+    // HSPI = CS: 15, CLK: 14, MOSI: 13, MISO: 12 -> default
+    // VSPI = CS:  5, CLK: 18, MOSI: 23, MISO: 19
+    slave.setDataMode(SPI_MODE0);
+    slave.begin();
+    // slave.begin(VSPI);   // you can use VSPI like this
+
+    // clear buffers
+    memset(spi_slave_tx_buf, 0, BUFFER_SIZE);
+    memset(spi_slave_rx_buf, 0, BUFFER_SIZE);
+}
+
+void loop() {
+    // block until the transaction comes from master
+    slave.wait(spi_slave_rx_buf, spi_slave_tx_buf, BUFFER_SIZE);
+
+    // if transaction has completed from master,
+    // available() returns size of results of transaction,
+    // and buffer is automatically updated
+
+    while (slave.available()) {
+        for (size_t i = 0; i < BUFFER_SIZE; ++i) {
+            printf("%d ", spi_slave_rx_buf[i]);
+        }
+        printf("\n");
+
+      if (spi_slave_rx_buf[1] == 0){
+        digitalWrite(22, LOW);
+      }else{
+        digitalWrite(22, HIGH);
+      }
+
+      slave.pop();
+    }
+}
